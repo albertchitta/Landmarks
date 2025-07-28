55ba69bf2c81ac03161e0b5808f0b19ccdf89e9d
Albert Chittaphong <albert.w.chittaphong@gmail.com>
2025-07-27 23:14:05 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Albert Chittaphong on 7/27/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
