d027a1d91539af86f20475e9667f60c425b6317c
Albert Chittaphong <albert.w.chittaphong@gmail.com>
2025-07-29 22:10:33 -0500

    Drawing paths and shapes

---
+++ Landmarks/Views/Helpers/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Albert Chittaphong on 7/27/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
